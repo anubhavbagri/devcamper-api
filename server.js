9797d3f668509d847d9ec8c3caa02bd6890372ca
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-11-21 17:38:22 +0530

    feat: sending basic response through express

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+  //   res.send('<h1>Hello from express</h1>');
+  // res.send({ name: 'Anubhav' });
+  // res.json({ name: 'Anubhav' });
+  // res.sendStatus(400)
+  // res.status(400).json({ success: false });
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
