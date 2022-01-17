8b7d32e26cf6f4c59f3e909d5510683e24370857
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-18 04:35:41 +0530

    fix: try catch block; feat: Delete request

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -59,16 +59,20 @@ exports.createBootcamp = async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  if (!bootcamp) {
-    return res.status(400).json({ success: false });
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
   }
-
-  res.status(200).json({ success: true, data: bootcamp });
 
   /*
   res
@@ -80,8 +84,21 @@ exports.updateBootcamp = async (req, res, next) => {
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+  /*
   res
     .status(200)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+    */
 };
