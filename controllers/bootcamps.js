221012fcd8123bda09086fb147c5a20c6c1befa3
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-18 16:24:15 +0530

    feat: custom ErrorResponse class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -6,7 +7,6 @@ const Bootcamp = require('../models/Bootcamp');
 exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
-
     res
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
@@ -24,21 +24,22 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
-
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      // to throw error if its a formatted object ID but not in our database
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
-
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(err);
+    // to throw error if its not a formatted object ID
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-  /*
-  res
+  /*res
     .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-    */
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });*/
 };
 
 // @desc    Create new bootcamp
